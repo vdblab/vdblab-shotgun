cdacc6877812e1d7b95d524ca88d32c9a3e3be0b
baichom1 <baichom1@mskcc.org>
2024-08-28 10:34:45 -0400

    fixing type of error message

--- workflow/scripts/qc/check_sortmerna_report.py
+++ workflow/scripts/qc/check_sortmerna_report.py
@@ -18,7 +18,7 @@ def main(exp_ids, logs, threshold, out_f):
             error_message = error_message + ["Error in sortmerna report for ",
                                exp_ids[i],
                                " value of sortmerna reads was: ",
-                               smr_df['mean_perc'], '\n']
+                               smr_df['mean_perc'].tolist()[0], '\n']
         if merged_df:
             merged_df = merged_df.append(smr_df, ignore_index=True)
         else:
@@ -26,7 +26,7 @@ def main(exp_ids, logs, threshold, out_f):
     merged_df.to_csv(out_f)
     with open(out_f, 'a') as o_f:
         print(error_message)
-        o_f.write(''.join(error_message))
+        o_f.write(''.join(list(error_message)))
 
 
 if __name__ == "__main__":
